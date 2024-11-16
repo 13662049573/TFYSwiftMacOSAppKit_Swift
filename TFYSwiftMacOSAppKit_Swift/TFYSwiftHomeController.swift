7ebc459f8741f4c45b2ae6165bf7186fcae03309
田风有 <420144542@qq.com>
2024-11-16 16:10:55 +0800

    a

--- TFYSwiftMacOSAppKit_Swift/TFYSwiftHomeController.swift
+++ TFYSwiftMacOSAppKit_Swift/TFYSwiftHomeController.swift
@@ -11,18 +11,19 @@ import Cocoa
 class TFYSwiftHomeController: NSViewController {
 
     override func loadView() {
-        let view:NSView = NSView(frame: NSRect(x: 0, y: 0, width: 1100, height: 720))
+        let view:NSView = NSView(frame: NSRect(x: 0, y: 0, width: 400, height: 600))
         self.view = view
     }
     
     lazy var textfiled: NSTextField = {
-        let filed = NSTextField(frame: NSRect(x: 100, y: 200, width: 300, height: 50))
+        let filed = NSTextField(frame: NSRect(x: 20, y: 200, width: self.view.macos_width-40, height: 50))
         filed.chain
             .font(NSFont.systemFont(ofSize: 14, weight: .semibold))
             .wantsLayer(true)
             .backgroundColor(.white)
             .text("sssssssss")
             .textColor(.red)
+            .focusRingType(.none)
         return filed
     }()
     
@@ -30,10 +31,34 @@ class TFYSwiftHomeController: NSViewController {
         super.viewDidLoad()
         // Do view setup here.
         
-        view.wantsLayer = true
-        view.layer?.backgroundColor = NSColor.white.cgColor
+//        view.wantsLayer = true
+//        view.layer?.backgroundColor = NSColor.white.cgColor
      
         view.addSubview(textfiled)
+        
+        let emitterLayer = CAEmitterLayer()
+        let cell = CAEmitterCell()
+        cell.contents = NSImage(named: "mood_background_1")?.cgImage
+        cell.birthRate = 10
+        cell.lifetime = 5
+        cell.velocity = 100
+        cell.scale = 0.5
+
+        emitterLayer.chain
+            .emitterCells([cell])
+            .birthRate(1.0)
+            .lifetime(2.0)
+            .emitterPosition(NSPoint(x: 100, y: 100))
+            .emitterSize(NSSize(width: 50, height: 50))
+            .emitterShape(.line)
+            .emitterMode(.surface)
+            .renderMode(.additive)
+            .velocity(100.0)
+            .scale(1.0)
+            .spin(2.0)
+            .preservesDepth(true)
+        
+        textfiled.layer?.addSublayer(emitterLayer)
     }
     
 }
